a71438987d10f2da11d75de946f798eb3ab7fa1e
ladygonzalez <ladymonrroy98@gmail.com>
2022-05-18 13:56:56 -0600

    ejercicio2

---
+++ ejercicio2.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Ejercicios
+{
+    class Ejer2
+    {
+        static void Main()
+        {
+            /*De acuerdo con la geometria de un triangulo se calcula aplicando la formula
+              Area = Base * Altura / 2. Realice una rutina que determine el area de un triángulo dado*/
+            float ba;
+            float altura;
+            float area;
+
+            Console.WriteLine("Ingre la base del Triángulo:");
+            ba = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingre la altura del Triángulo:");
+            altura = float.Parse(Console.ReadLine());
+
+            area = (ba * altura) / 2;
+
+            Console.WriteLine("El área del triángulo es: {0}", area);
+
+            Console.ReadLine();
+        }
+    }
+}
+
